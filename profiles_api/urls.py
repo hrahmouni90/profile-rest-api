c0acd887c5b11b8f9bc73bdf79ef106720797147
Hicham Rahmouni <hicrahtubi@gmail.com>
2022-10-26 14:15:27 +0300

    Added login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,11 +5,14 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
+
+
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,13 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class HelloApiView(APIView):
